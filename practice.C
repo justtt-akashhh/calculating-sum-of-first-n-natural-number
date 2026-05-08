9dea8c06173a3c64c2e438235487d6fe600f74f9
Akash Tripathi <vipakashtripathi124421@gmail.com>
2026-05-08 22:57:03 +0530

    Enhance practice.C with comments for clarity
    Added comments to clarify user input and loop functionality.

--- practice.C
+++ practice.C
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    printf("Enter a number: ");
+    int a;                        
+    printf("Enter a number: ");        //Taking input from the user
     scanf("%d",&a);
 
     int sum = 0;
-    for (int i = 0; i <= a; i++){
+    for (int i = 0; i <= a; i++){         //for loop for calculation
     sum = sum + i;}
-    printf("The sum of the first %d natural numbers is: %d", a, sum);
+    printf("The sum of the first %d natural numbers is: %d", a, sum);        // shows output to the user
     return 0;
 }    
